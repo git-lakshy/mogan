86e4f9d63e84c132a16ca6f8afa7cfb58917d6af
Divyansh Sharma <140371139+divyansharma001@users.noreply.github.com>
2026-02-26 13:13:16 +0530

    [202_105]: Replace Meta with platform-appropriate key name in shortcut … (#2856)
    Co-authored-by: divyansharma001 <divyansharma001@users.noreply.github.com>

--- src/Texmacs/Server/tm_config.cpp
+++ src/Texmacs/Server/tm_config.cpp
@@ -264,10 +264,12 @@ system_kbd_initialize (hashmap<string, tree>& h) {
     h ("<gtr>")    = "<#3E>";
   }
   else if (gui_is_qt ()) {
-    h ("S-")       = localize ("Shift::keyboard", true);
-    h ("C-")       = localize ("Ctrl::keyboard", true);
-    h ("A-")       = localize ("Alt::keyboard", true);
-    h ("M-")       = localize ("Meta::keyboard", true);
+    h ("S-")= localize ("Shift::keyboard", true);
+    h ("C-")= localize ("Ctrl::keyboard", true);
+    h ("A-")= localize ("Alt::keyboard", true);
+    if (os_win ()) h ("M-")= localize ("Win::keyboard", true);
+    else if (os_macos ()) h ("M-")= localize ("Command::keyboard", true);
+    else h ("M-")= localize ("Super::keyboard", true);
     h ("H-")       = localize ("Hyper::keyboard", true);
     h ("windows")  = localize ("Windows::keyboard");
     h ("capslock") = localize ("Capslock::keyboard");
